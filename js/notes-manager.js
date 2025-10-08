2318e379d6c6e70a7deac9ff2307acfe028b1b99
meha5494-ai <meha5494@gmail.com>
2025-10-08 19:42:20 +0330

    Update notes-manager.js

--- js/notes-manager.js
+++ js/notes-manager.js
@@ -1,7 +1,7 @@
-export class NotesManager {
-    constructor() { this.notes=[]; }
+export class NotesManager{
+    constructor(){ this.notes=[]; }
     addNote(note){ if(note && typeof note==='string') this.notes.push(note); }
     getNotes(){ return this.notes; }
-    removeNote(index){ if(index>=0 && index<this.notes.length) this.notes.splice(index,1); }
+    removeNote(i){ if(i>=0 && i<this.notes.length) this.notes.splice(i,1); }
 }
 export const notesManagerInstance = new NotesManager();
